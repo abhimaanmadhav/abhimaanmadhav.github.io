5224ffcd1319aca8da59891a5dbd10451346cd86
Abhimaan Madhav <abhimaan@techjini.com>
2015-10-24 20:38:15 +0530

    new theme website

---
+++ older/javascripts/googlemap.js
@@ -0,0 +1,36 @@
+var directionsDisplay ;
+var directionsService;
+var map;
+
+function initMap(){
+    var myOptions = {
+        zoom: 10,
+        mapTypeId: google.maps.MapTypeId.ROADMAP,
+        center: new google.maps.LatLng(12.3025598,76.6196739)
+    };
+    map = new google.maps.Map(document.getElementById("map_canvas"), myOptions);
+	directionsDisplay = new google.maps.DirectionsRenderer({ draggable: true });
+	directionsDisplay.setPanel(document.getElementById("directions"));
+      directionsService = new google.maps.DirectionsService();
+    $("#routeMode").on("change", function() { calcRoute(); });
+    $("#routeGo").on("click", function() { calcRoute(); });
+    $("#routeClear").on("click", function() { directionsDisplay.setDirections({ routes: [] }); });
+    directionsDisplay.setMap(map);
+    
+
+    
+};
+
+
+function calcRoute() {
+    var request = {
+        origin: $("#routeFrom").val(),
+        destination: new google.maps.LatLng(12.3025598,76.6196739),
+        travelMode: google.maps.TravelMode[$("#routeMode").val()]
+    };
+    directionsService.route(request, function(response, status) {
+        if (status == google.maps.DirectionsStatus.OK) {
+            directionsDisplay.setDirections(response);
+        }
+    });
+}
